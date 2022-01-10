d59c1ffc2dc686b58599f8974bc298b4ada2d21a
MacroHEX <steam18py@hotmail.com>
2022-01-10 15:53:17 -0300

    Prueba de nuevo menú con diccionarios

---
+++ EstudioUdemy/NewMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy
+{
+    public class NewMenu
+    {
+        public void PrintM(string n)
+        {
+            HWList hWList = new HWList();
+            Console.Clear();
+            Dictionary<string, HWList> Diccionariol = new Dictionary<string, HWList>()
+            {
+                {"1", hWList},
+            };
+            hWList[0].Main(HW01ex01);
+        }
+    }
+}
+
+Console.
