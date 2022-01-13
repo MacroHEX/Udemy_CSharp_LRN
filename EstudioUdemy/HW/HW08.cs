454f85976884d6b5e3305ba587f4d10e52647f07
MacroHEX <steam18py@hotmail.com>
2022-01-12 22:32:07 -0300

    Colecciones genericas (List)

---
+++ EstudioUdemy/HW/HW08.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy
+{
+    class HW08
+    {
+        public static void Main()
+        {
+            bool run = true;
+
+
+            List<string> clientes_name = new List<string>();
+            List<string> clientes_surname = new List<string>();
+            List<string> clientes_ci = new List<string>();
+
+            while (run)
+            {
+                Console.Clear();
+                Console.WriteLine("Lista de Clientes del Banco.\n");
+                Console.WriteLine("1. Añadir Usuario.");
+                Console.WriteLine("2. Eliminar Usuario.");
+                Console.WriteLine("3. Listar Usuarios.");
+                Console.WriteLine("4. Salir.");
+                Console.Write("\n>> ");
+                Console.ReadKey();
+
+            }
+        }
+    }   
+}
